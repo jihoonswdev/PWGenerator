fbe166362f9d93dee6c0a9016086a89c6bb7cdb4
Jason Park <jihoonswdev@gmail.com>
2021-08-07 23:19:12 -0500

    homework update

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # PWGenerator
+
+* Hello
+## Hello
+
+##### Hello
+
+**Hello**
+
+***Hello***
+
+
+```
+git add .
+git commit -m "Your commit message here"
+```
+
+# This project used
+- Javascript
+- Css
+- HTML
+
+1. James
+2. Manoli
+3. Adwait
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,69 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var specialChars = "!@#$%^&*";
+var numericalChars = "0123456789";
+var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordResults = "";
+var userOptions = "";
 
-  passwordText.value = password;
+function generatePassword() {
+  var passLength = prompt("How many characters do you want as your password?");
+console.log(passLength);
 
+  if (passLength < 8 || passLength > 128) {
+    alert("You have to have at least 8 characters long and lower than 128 characters");
+    return;
+  }
+
+var useLowerCase = confirm("Do you want to use lower case?");
+var useUpperCase = confirm("Do you want to use upper case?");
+var useSpecialChars = confirm("Do you want to use special chars?");
+var useNumerialChars = confirm("Do you want to use numerical chars?");
+
+if (
+  useLowerCase === false &&
+  useUpperCase === false &&
+  useSpecialChars === false &&
+  useNumerialChars === false
+) {
+  alert("You must choose at least one character type!");
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+if (useLowerCase === true) {
+userOptions = userOptions.concat(lowerCaseChars)
+}
+if (useUpperCase === true) {
+userOptions = userOptions.concat(upperCaseChars)
+}
+if (useSpecialChars === true) {
+userOptions = userOptions.concat(specialChars)
+}
+if (useNumerialChars ===true) {
+userOptions = userOptions.concat(numericalChars)
+}
+
+console.log(userOptions)
+
+for (var i=0; i < passLength; i++) {
+  // var password = math.floor(userOptions * Math.random());
+// userOptions.push (pwResults[Math.floor(Math.random() * pwResults.length)]);
+//   pwResults += userOptions.charAt(password);
+var password = Math.floor(userOptions.length * Math.random())
+
+   passwordResults += userOptions.charAt(password)
+
+}
+// return userOptions.join("");
+document.getElementById("password").textContent = passwordResults;
+}
+
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+// }
+// document.getElementById("password").textContent = pwResults;
+
+generateBtn.addEventListener("click", generatePassword);
